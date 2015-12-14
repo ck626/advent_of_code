b1608573d82fdef02a7aff62e6d5d078f74403c6
Corey A. Kazaks <ck626@live.com>
2015-12-14 17:18:32 -0500

    completed day 1 part 2

--- day_01.rb
+++ day_01.rb
@@ -2,16 +2,22 @@
 file = File.open("day_01.txt", "r")
 contents = file.read
 contents = contents.split(/()/)
+contents = contents.select {|e| e != ""}
 
 floor = 0
+position = []
 
-contents.each do |c|
+contents.each_with_index do |c, i|
   if c == "("
     floor += 1
   elsif c == ")"
     floor -= 1
-  else
+    if floor == -1
+      position << i+1
+    end
   end
 end
 
 puts "Santa is on floor #{floor}"
+
+puts "Santa entered the basement at position #{position[0]}"
