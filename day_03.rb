f9d34720c74b0786e1575dd92cd091fbeaf70cee
Corey A. Kazaks <ck626@live.com>
2015-12-18 17:09:42 -0500

    updat

--- day_03.rb
+++ day_03.rb
@@ -1,4 +1,6 @@
 # Advent of code - day 03 part 01 completed
+
+# Part 1
 directions = File.read('day_03.txt')
 
 current_x = 0
@@ -17,6 +19,33 @@ directions.each_char do |e|
     current_x += 1
   end
   houses[ [current_x,current_y] ] = "visited"
+end
+
+puts houses.length
+
+# Part 2
+
+directions = File.read('day_03.txt')
+# Found out about cycle and learned from this site:
+# http://www.sitepoint.com/guide-ruby-collections-iii-enumerable-enumerator/
+both_santas_arr = [[0,0],[0,0]].cycle
+
+houses = { [0,0] => "visited" }
+
+directions.each_char do |e|
+  santa = both_santas_arr.next
+  case e
+  when 'v'
+    santa[1] += 1
+  when '<'
+    santa[0] -= 1
+  when '^'
+    santa[1] -= 1
+  when '>'
+    santa[0] += 1
+  end
+
+  houses[ [santa[0], santa[1]] ] = "visited"
 end
 
 puts houses.length
