1dd52591f9384af5b9d3ba1368b5d576f1a141d6
Corey A. Kazaks <ck626@live.com>
2015-12-18 12:22:06 -0500

    day 2 part 2 completed

--- day_02.rb
+++ day_02.rb
@@ -1,6 +1,7 @@
-# Advent of code - day 02 - Part 1 complete
+# Advent of code - day 02 - Parts 1,2 complete
 surface_area_total = 0.0
 small_sides_total = 0.0
+ribbon_total = 0.0
 
 file = File.new('day_02.txt', 'r')
 contents = file.read
@@ -10,20 +11,29 @@ contents.each do |e|
   l = e[0].to_i
   w = e[1].to_i
   h = e[2].to_i
-  # sides
+  # sides vars
   a = l*w
   b = w*h
   c = h*l
-
-  sa = 2*l*w + 2*w*h + 2*h*l
+  # perimeters
+  pa = 2*l + 2*w
+  pb = 2*w + 2*h
+  pc = 2*h + 2*l
+  # surface area
+  sa = 2*a + 2*b + 2*c
+  # volume
+  v = l*w*h
+  # additions to totals
   surface_area_total += sa
 
   sides_arr = [a,b,c]
   small_side = sides_arr.min
   small_sides_total += small_side
 
+  perimeters_arr = [pa,pb,pc]
+  small_perimeter = perimeters_arr.min
+  ribbon_total += small_perimeter + v
 end
 
-puts surface_area_total
-puts small_sides_total
 puts surface_area_total + small_sides_total
+puts ribbon_total
